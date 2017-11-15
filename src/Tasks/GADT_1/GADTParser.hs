1e932a0fd362262983a8819e63293e51a74b56ec
aga-ad <aganov.a.d@gmail.com>
2017-11-15 21:33:43 +0300

    the most pretty version

--- src/Tasks/GADT_1/GADTParser.hs
+++ src/Tasks/GADT_1/GADTParser.hs
@@ -34,28 +34,23 @@ leqP = Leq <$> (parse <* char '<') <*> parse
 andP :: Parser (Expr Bool)
 andP = And <$> ((makeParser [bbLitP, leqP]) <* string "&&") <*> parse
 
-bracketP :: Parser a -> Parser a
-bracketP = between (char '(') (char ')')
-
-tsP :: Parser a -> Parser a
-tsP p = try $ spacedP p where
-  spacedP :: Parser a -> Parser a
-  spacedP p = (many space *> p) <* many space
-
-unfolderP :: [Parser a] -> Parser a
-unfolderP lp = foldl (<|>) (head lp) (tail lp)
-
-
-
-class MyParse a where
-  parse :: Parser (Expr a)
-  parse = makeParser funcs
-  funcs :: [Parser (Expr a)]
-
-  makeParser :: [Parser (Expr a)] -> Parser (Expr a)
-  makeParser lp = unfolderP $ ([tsP] <*> lp) ++ ([\p -> tsP $ bracketP $ tsP p] <*> funcs)
-
 instance MyParse Int where
   funcs = [addP, iiLitP]
 instance MyParse Bool where
   funcs = [andP, leqP, bbLitP]
+
+
+class MyParse a where
+  funcs :: [Parser (Expr a)]
+
+  parse :: Parser (Expr a)
+  parse = makeParser funcs
+
+  makeParser :: [Parser (Expr a)] -> Parser (Expr a)
+  makeParser lp = alternativerP $ ([tsP] <*> lp) ++ ([\p -> tsP $ bracketP $ tsP p] <*> funcs) where
+    bracketP = between (char '(') (char ')')
+    alternativerP [x] = x
+    alternativerP (x:xs) = x <|> alternativerP xs
+    tsP p = try $ spacedP p
+    spacedP :: Parser a -> Parser a
+    spacedP p = (many space *> p) <* many space
